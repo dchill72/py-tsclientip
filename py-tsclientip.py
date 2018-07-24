2a2b4a459543875961c66f3fd3229dda03f560c2
David Hill <david.hill@tanium.com>
2018-07-23 21:58:19 -0400

    * make plugin directory a parameter

--- py-tsclientip.py
+++ py-tsclientip.py
@@ -15,20 +15,19 @@ def main():
     ts_server = sys.argv[1]
     ts_admin = sys.argv[2]
     ts_pwd = sys.argv[3]
+    ts_plugin = sys.argv[4]
 
     ip_address = getMyIp()
-
-    script_text = "$ip_address = '" + ip_address + """'\n$path = @'C:\\Program Files\\Tanium\\Tanium Module Server\\
-    plugins\\content\\integrity-monitor-scheduledtask\\invoker.js'@
+#    plugins\\content\\""" + ts_plugin + """\\invoker.js'@
+    script_text = "$ip_address = '" + ip_address + """'\n$path = 'C:\\Program Files\\Tanium\\Tanium Module Server\\plugins\\content\\""" + ts_plugin + """\\invoker.js'
 $hostname = "hostname: '"
 $port = "port: "
 
-$text1 = Get-Content $path
-$text1 -replace ("(?<=$hostname)" + "(.*?)(?=\')"), "$ip_address" | Set-Content $path
-$text2 = Get-Content $path
-$text2 -replace ("(?<=$port)" + "(.*?)(?=,)"), "17456" | Set-Content $path
-Write-Host "Done"
-Exit"""
+$text = Get-Content $path
+$text -replace ("(?<=$hostname)" + "(.*?)(?=\')"), $ip_address | Set-Content $path
+$text = Get-Content $path
+$text -replace ("(?<=$port)" + "(.*?)(?=,)"), "17456" | Set-Content $path
+Write-Host 'Done'"""
 
     print(script_text)
     s = winrm.Session(ts_server, auth=(ts_admin, ts_pwd))
@@ -36,8 +35,7 @@ Exit"""
     r = s.run_ps(script_text)
     print("Script complete")
 
-    print(r.status_code)
-    print(r.std_out)
+    print 'Result: ' + r.std_out
 
 
 if __name__ == "__main__":
