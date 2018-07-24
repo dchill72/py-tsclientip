1affbf8c1f3631df14cdea0c95875d1c5e939b28
David Hill <david.hill@tanium.com>
2018-07-24 09:01:20 -0400

    make it shine

--- py-tsclientip.py
+++ py-tsclientip.py
@@ -3,12 +3,17 @@ import netifaces
 import sys
 
 
-def getMyIp():
+def get_ip():
+    # making assumption that utun1 is the vpn interface
+    # todo: validate this assumption?
     if 'utun1' in netifaces.interfaces():
         return netifaces.ifaddresses('utun1')[netifaces.AF_INET][0]['addr']
 
+    # the primary gateway provides the default interface name for this machine
     primaryInteface = netifaces.gateways()['default'][netifaces.AF_INET][1]
-    return netifaces.ifaddresses(primaryInteface)[netifaces.AF_INET][0]['addr']
+    ip_address = netifaces.ifaddresses(primaryInteface)[netifaces.AF_INET][0]['addr']
+    print primaryInteface + ' IP: ' + ip_address
+    return ip_address
 
 
 def main():
@@ -17,8 +22,10 @@ def main():
     ts_pwd = sys.argv[3]
     ts_plugin = sys.argv[4]
 
-    ip_address = getMyIp()
-#    plugins\\content\\""" + ts_plugin + """\\invoker.js'@
+    ip_address = get_ip()
+
+    # this string format is the most brittle part of the script, changing it
+    # can easily break the remote call for no apparent reason
     script_text = "$ip_address = '" + ip_address + """'\n$path = 'C:\\Program Files\\Tanium\\Tanium Module Server\\plugins\\content\\""" + ts_plugin + """\\invoker.js'
 $hostname = "hostname: '"
 $port = "port: "
@@ -29,13 +36,16 @@ $text = Get-Content $path
 $text -replace ("(?<=$port)" + "(.*?)(?=,)"), "17456" | Set-Content $path
 Write-Host 'Done'"""
 
-    print(script_text)
+    print 'Remote Script:'
+    print '--------------------------------------------------------------------------------------------'
+    print script_text
+    print '--------------------------------------------------------------------------------------------'
     s = winrm.Session(ts_server, auth=(ts_admin, ts_pwd))
-    print("Running script")
+    print 'Running script'
     r = s.run_ps(script_text)
-    print("Script complete")
+    print 'Script complete'
 
-    print 'Result: ' + r.std_out
+    print 'StdOut From Remote: ' + r.std_out
 
 
 if __name__ == "__main__":
